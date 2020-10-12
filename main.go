cf34fa4e18bf9d2f6f391f361c97231f05a73ef3
David <david_xd1996@live.com.mx>
2020-10-12 13:40:33 -0500

    primer commit

--- main.go
+++ main.go
@@ -2,6 +2,20 @@ package main
 
 import "fmt"
 
-func main()  {
-	fmt.Println("Hola Mundo!") // eliminar esta línea
+func main() {
+	var value, tmp int
+
+	fmt.Scan(&value)
+
+	s := make([]int, value)
+
+	for i := 0; i < value; i++ {
+		fmt.Scan(&s[i])
+	}
+
+	for _, v := range s {
+		tmp += v
+	}
+
+	fmt.Println(tmp)
 }
